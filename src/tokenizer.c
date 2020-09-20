eb7d6224666e0447e808182ef14acde28d0a9000
emontes22 <ervey025@gmail.com>
2020-09-19 20:45:05 -0600

    More methods added to tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -18,7 +18,7 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
-  while(space_char(*str)
+  while(space_char(*str))
     {
       if(non_space_char(*str))
 	return str;
@@ -29,7 +29,25 @@ char *word_start(char *str)
 
 char *word_terminator(char *word)
 {
-  
+  while(space_char(*word))
+    {
+      if(non_space_char(*word))
+	return word;
+      *word++;
+    }
+    return word;
+}
+
+int count_words(char *str)
+{
+  int count = 0;
+
+  for(int i = 0; str[i] != '\0'; i++)
+    {
+      if(space_char(str[i]))
+	count++;
+    }
+  return count;
 }
 
 
