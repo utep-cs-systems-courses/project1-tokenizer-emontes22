4739e612ca311ea765266ff2728e05771d61bd15
emontes22 <ervey025@gmail.com>
2020-09-27 21:37:27 -0600

    Fixed some typos.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
+
+// int main(){
+//   printf("%c",tokenize("Hello world"));
+//   return 0;
+// }
 
 int space_char(char c)
 {
@@ -20,46 +26,46 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
-  while(space_char(*str))
-    {
-      str++;
-    }
-    return str;    
+  while (space_char(*str))
+  {
+    str++;
+  }
+  return str;
 }
 
 char *word_terminator(char *word)
 {
-  while(non_space_char(*word))
-    {
-      *word++;
-    }
-    return word;
+  while (non_space_char(*word))
+  {
+    *word++;
+  }
+  return word;
 }
 
 int count_words(char *str)
 {
   int count = 0;
 
-  for(int i = 0; str[i] != '\0'; i++)
-    {
-      if(space_char(str[i]))
-	count++;
-    }
+  for (int i = 0; str[i] != '\0'; i++)
+  {
+    if (space_char(str[i]))
+      count++;
+  }
   return count;
 }
 
 char *copy_str(char *inStr, short len)
 {
-  char *temp = (char *) malloc(len + 1);
-  
+  char *temp = (char *)malloc(len + 1);
+
   while (*inStr != '\0')
-    {
-      *temp = *inStr;
-      temp++;
-      inStr++;
-    }
+  {
+    *temp = *inStr;
+    temp++;
+    inStr++;
+  }
   *temp = '\0';
-  
+
   return temp;
 }
 
@@ -68,33 +74,33 @@ char **tokenize(char *str)
   int count = count_words(str);
   char **tokens = (char **)malloc((count + 1) * (sizeof(char *)));
   char *temp = str;
-  
+
   for (int i = 0; i < count; i++)
-    {
-      temp = word_start(temp);
-      tokens[i] = copy_str(temp, (word_terminator(temp) - word_start(temp)));
-      temp = word_terminator(temp);
-    }
+  {
+    temp = word_start(temp);
+    tokens[i] = copy_str(temp, (word_terminator(temp) - word_start(temp)));
+    temp = word_terminator(temp);
+  }
   return tokens;
 }
 
-void print_tokens(char **str)
+void print_tokens(char **tokens)
 {
-  while (*str!="")
-    {
-      printf("%s\n", *str);
-      str++;
-    }
+  while (*tokens != "")
+  {
+    printf("%s\n", *tokens);
+    tokens++;
+  }
 }
 
-void free_tokens(char **str)
+void free_tokens(char **tokens)
 {
-  char **temp = str;
-  
+  char **temp = tokens;
+
   while (*temp != "")
-    {
-      free(*temp);
-      temp++;
-    }
+  {
+    free(*temp);
+    temp++;
+  }
   free(temp);
 }
