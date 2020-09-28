3f768d0f775c82c625395fa54a642940a3516a5c
emontes22 <ervey025@gmail.com>
2020-09-27 21:49:56 -0600

    Simple UI.

--- src/ui.c
+++ src/ui.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+#include "history.h"
+
 //Simple UI to let the user enter a string
 int main()
 {
-    char str[] = "";
-    printf("Enter a string: ");
-    scanf("%s", str);
+  char input[50];
+  List *history = init_history();
+
+  printf("Welcome, Please select from the following options:\n");
+  printf("'1' To tokenize a sentence.\n'2' To view the history.\n'3' To quit.\n");
+  fgets(input, 50, stdin);
+
+  int choice = atoi(input);
+
+  switch (choice)
+  {
+  case 1:
+    printf("Enter a sentence to tokenize:\n");
+    fgets(input, 50, stdin);
+    add_history(history, input);
+    char **tokens = tokenize(input);
+    print_tokens(tokens);
+    break;
+
+  case 2:
+    print_history(history);
+    break;
+
+  case 3:
+    exit(0);
+
+  default:
+    exit(0);
+    break;
+  }
 }
